0e39a92afa07cd4dbcdae1e601d28926441342b6
Vuqar111 <vuqar.hesenov_2003@mail.ru>
2023-10-14 22:39:58 +0400

    mib

--- controllers/statisticsController.js
+++ controllers/statisticsController.js
@@ -21,3 +21,28 @@ export const generalStatistics = async (req, res) => {
     res.status(500).send({ message: "Error during login" });
   }
 };
+
+
+
+export const analytics = async (req, res) => {
+  try {
+    const gas = false;
+    const rain = 10;
+    const light = 15;
+    const humidity = 20;
+    const temperature = 25;
+    const soil = 30;
+
+    res.send({
+      gas,
+      rain,
+      light,
+      humidity,
+      temperature,
+      soil,
+    });
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).send({ message: "Error during login" });
+  }
+}
